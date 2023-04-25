864a1bf1a5ca35525eb53052385e87f3432ab3f4
TianxuLuo <TianxuLuo>
2023-04-25 20:57:21 +0000

    Add the Framework to use Prophet

--- final_project/03 mdl.py
+++ final_project/03 mdl.py
@@ -13,6 +13,74 @@ print("YOUR CODE HERE...")
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Modeling and MLOps
+# MAGIC * Considering historical data, build a forecasting model that infers net bike change at your station by the hour
+# MAGIC * Register your model at the staging and production level within the Databricks model registry.
+# MAGIC * Store artifacts in each MLflow experiment run to be used by the application to retrieve the staging and production models.
+# MAGIC * Tune the model hyperparameters using the MLflow experiments and hyperparameter scaling (spark trials, hyper-opts).
+# MAGIC * **YOU MUST NAME YOUR GROUP MODEL IN THE MLFLOW REGISTRY WITH YOUR GROUP’S UNIQUE NAME. SEE GROUP_MODEL_NAME GLOBAL VARIABLE.**
+
+# COMMAND ----------
+
+# Import libraries
+import mlflow
+import json
+import pandas as pd
+import numpy as np
+from prophet import Prophet, serialize
+from prophet.diagnostics import cross_validation, performance_metrics
+
+# Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+# Hyperparameter tuning
+import itertools
+
+# COMMAND ----------
+
+# Read the data from the silver table
+SOURCE_DATA = ("")
+ARTIFACT_PATH = "G10-model"
+np.random.seed(12345)
+
+## Helper routine to extract the parameters that were used to train a specific instance of the model
+def extract_params(pr_model):
+    return {attr: getattr(pr_model, attr) for attr in serialize.SIMPLE_ATTRIBUTES}
+
+
+sales_data = pd.read_csv(SOURCE_DATA)
+print(f"{len(sales_data)} months of sales data loaded ({round(len(sales_data)/12,2)} years)")
+
+# Visualize data using seaborn
+sns.set(rc={'figure.figsize':(12,8)})
+sns.lineplot(x=sales_data['ds'], y=sales_data['y'])
+plt.legend(['Sales Data'])
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
 import json
 
 # Return Success
